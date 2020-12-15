85d738f197562b798d0caa42cd0573b602e754e0
Deepak Gautam <anin0543@edu.easj.dk>
2020-12-15 22:23:31 +0100

    dsfsd

--- server.js
+++ server.js
@@ -23,13 +23,7 @@ app.use(express.static(path.join(__dirname, 'client/dist/')))
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'client/dist/client/index.html'));
   })
-  if (process.env.NODE_ENV === "production") {
-    app.use(express.static('client/build'));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-    })
-}
 
 (userRoute)(app);
 (checkin)(app);
