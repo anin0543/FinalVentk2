d9720482bfca4f69d55c6753a67645b2626bd484
Deepak Gautam <anin0543@edu.easj.dk>
2020-12-15 01:01:38 +0100

    minor change

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ if (process.env.NODE_ENV === "production") {
 require("./app/routes/user.routes")(app);
 require("./app/routes/checkin.routes")(app);
 require("./app/routes/auth.routes")(app);
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
